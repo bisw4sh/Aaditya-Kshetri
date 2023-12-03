de41dcce304004972dbc7450330348dd97981cb8
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-12-03 10:42:21 +0545

    contact added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Personal Portfolio 
-- Aaditya Kshetri
+ Aaditya Kshetri
 ### To get started
 ```bash
-git clone https://github.com/bisw4sh/Saroj-Dhungana.git
+git clone https://github.com/bisw4sh/Aaditya-Kshetri.git
 ```
---
+++ pages/Contact.js
@@ -0,0 +1,22 @@
+document
+  .getElementById("form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get input values
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    // Compose email body
+    const subject = `New message from ${name}`;
+    const body = `From: ${name}\nEmail: ${email}\n\nMessage: ${message}`;
+
+    // Create mailto link
+    const mailtoLink = `mailto:aadityakshetri676@gmail.com?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+
+    // Open default mail application
+    window.location.href = mailtoLink;
+  });
